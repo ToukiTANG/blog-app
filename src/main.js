164a61bd441e1ffee0109b3701b9e85af6b0c264
Touki <Laptop@Touki.com>
2022-07-12 19:10:07 +0800

    更改了时间工具的引入形式

--- src/main.js
+++ src/main.js
@@ -4,18 +4,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import store from '@/store/index'
-import {timeToNow, timeFormat} from "@/utils/timeFormat";
+
+import "@/utils/timeFormat.js"
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
-Vue.filter("timeToNow", function (time) {
-    return timeToNow(time)
-})
-
-Vue.filter("timeFormat", function (time, format = "YYYY-MM-DD HH:mm:ss") {
-    return timeFormat(time, format)
-})
 
 new Vue({
     render: h => h(App),
--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -1,18 +1,18 @@
+import Vue from 'vue'
 // http://momentjs.cn/
 import moment from 'moment'
 
 //设置moment国际化语言
 moment.locale('zh-cn')
 
-export function timeToNow(time) {
+Vue.filter('timeFormat', function (value, format = 'YYYY-MM-DD HH:mm:ss') {
+    return moment(value).format(format)
+})
+
+Vue.filter('timeToNow', function (value) {
     //相对时间大于一个月，显示详细时间
-    if (moment().diff(moment(time), "days") > 7) {
-        return moment(time).format("YYYY-MM-DD HH:mm")
+    if (moment().diff(moment(value),"days") > 7) {
+        return moment(value).format('YYYY-MM-DD HH:mm')
     }
-    return moment(time).fromNow()
-}
-
-export function timeFormat(time, format = "YYYY-MM-DD HH:mm:ss") {
-    return moment(time).format(format)
-}
-
+    return moment(value).fromNow()
+})
